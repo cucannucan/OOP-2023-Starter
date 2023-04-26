2d34c48a823cb2eddc9c6e614c6352fb53aedc7e
cucannucan <cerenucan02@gmail.com>
2023-04-26 17:39:13 +0100

    final code with comments in it

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -1,3 +1,5 @@
+//Ceren Ucan D21124013
+//This lab test is about creating a sonnet by using the words from the shakespere.txt file.
 package ie.tudublin;
 
 import java.util.ArrayList;
@@ -10,7 +12,6 @@ public class DANI extends PApplet {
 
 	public void settings() {
 		size(1024, 1000);
-	
 	}
 
 	public void setup() {
@@ -46,6 +47,7 @@ public class DANI extends PApplet {
 		}
 	}
 
+	// This method takes a file name as input and loads the text from that file into memory. 
 	public void loadFile(String filename) {
 		String[] lines = loadStrings(filename);
 		for (String line : lines) {
@@ -71,7 +73,9 @@ public class DANI extends PApplet {
 		}
 	}
 
+	//This method searches through the model object for a Word object with the given string value. If it finds a matching Word object, it returns that object; otherwise, it returns null.
 	public Word findWord(String str) {
+
 		for (Word word : model) {
 			if (word.getWord().equals(str)) {
 				return word;
@@ -80,6 +84,7 @@ public class DANI extends PApplet {
 		return null;
 	}
 
+	//This method prints out the model object by iterating over each Word object in the list and printing its word value and list of followers.
 	public void printModel() {
 		for (Word word : model) {
 			System.out.print(word.getWord() + ": ");
@@ -90,6 +95,7 @@ public class DANI extends PApplet {
 		}
 	}
 
+	//his method generates a sonnet using the model object by randomly selecting a starting Word object from the list and then iterating through its list of followers to choose the next word in each line of the sonnet. It repeats this process for 14 lines to generate a full sonnet, which it returns as an array of strings.
 	public String[] writeSonnet() {
 		String[] sonnet = new String[14];
 		for (int i = 0; i < sonnet.length; i++) {
@@ -111,6 +117,7 @@ public class DANI extends PApplet {
 	}
 }
 
+//This is a helper class that represents a single follower word and its count in the Word object's list of followers.
 class Follow {
 	private String word;
 	private int count;
@@ -137,6 +144,7 @@ class Follow {
 	}
 }
 
+//This is a class that represents a single word and its list of followers in the model object. It has methods to add a follower to its list, retrieve its list of followers, and convert the Word object to a string for printing.
 class Word {
 	private String word;
 	private ArrayList<Follow> follows;
